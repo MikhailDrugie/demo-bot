50b37a403153dda1513b4e0c96e2142f021833d4
Mikhail Drugie <90636583+MikhailDrugie@users.noreply.github.com>
2021-11-19 10:43:38 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,10 +3,10 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 import messages
 
-inline_address = InlineKeyboardButton('Адрес', callback_data='address')
-inline_about_us = InlineKeyboardButton('О нас', callback_data='about_us')
-inline_contacts = InlineKeyboardButton('Контакты', callback_data='contacts')
-inline_site = InlineKeyboardButton('Наш сайт', url='mdprojecttg.ru')
+inline_address = InlineKeyboardButton('Address', callback_data='address')
+inline_about_us = InlineKeyboardButton('About us', callback_data='about_us')
+inline_contacts = InlineKeyboardButton('Contacts', callback_data='contacts')
+inline_site = InlineKeyboardButton('Our website', url='mdprojecttg.ru')
 
 inline_kb_info = InlineKeyboardMarkup(row_width=3).add(inline_about_us).add(inline_site).add(inline_address).add(
     inline_contacts)
